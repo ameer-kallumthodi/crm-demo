c6570cac4f227c0f8df8b090d96ff442fca042bf
Ameer Suhail <ameersuhail2001@gmail.com>
2025-11-19 12:23:01 +0530

    pdf error

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -512,35 +512,60 @@ class PaymentController extends Controller
      */
     private function numberToWords($number)
     {
-        $ones = array(
+        // Normalize value and support decimal (paise) amounts stored as strings
+        $number = (float) $number;
+        $number = round($number, 2);
+
+        $rupees = (int) floor($number);
+        $paise = (int) round(($number - $rupees) * 100);
+
+        $words = $this->convertNumberPortionToWords($rupees);
+
+        if ($paise > 0) {
+            $words .= ' and ' . $this->convertNumberPortionToWords($paise) . ' Paise';
+        }
+
+        return trim($words);
+    }
+
+    /**
+     * Recursively convert integer portion to words.
+     */
+    private function convertNumberPortionToWords(int $number): string
+    {
+        $ones = [
             0 => 'Zero', 1 => 'One', 2 => 'Two', 3 => 'Three', 4 => 'Four', 5 => 'Five',
             6 => 'Six', 7 => 'Seven', 8 => 'Eight', 9 => 'Nine', 10 => 'Ten',
             11 => 'Eleven', 12 => 'Twelve', 13 => 'Thirteen', 14 => 'Fourteen', 15 => 'Fifteen',
             16 => 'Sixteen', 17 => 'Seventeen', 18 => 'Eighteen', 19 => 'Nineteen'
-        );
-        
-        $tens = array(
+        ];
+
+        $tens = [
             20 => 'Twenty', 30 => 'Thirty', 40 => 'Forty', 50 => 'Fifty',
             60 => 'Sixty', 70 => 'Seventy', 80 => 'Eighty', 90 => 'Ninety'
-        );
-        
-        $hundreds = array(
-            100 => 'Hundred', 1000 => 'Thousand', 100000 => 'Lakh', 10000000 => 'Crore'
-        );
-        
+        ];
+
         if ($number < 20) {
             return $ones[$number];
-        } elseif ($number < 100) {
-            return $tens[10 * floor($number / 10)] . ($number % 10 ? ' ' . $ones[$number % 10] : '');
-        } elseif ($number < 1000) {
-            return $ones[floor($number / 100)] . ' Hundred' . ($number % 100 ? ' ' . $this->numberToWords($number % 100) : '');
-        } elseif ($number < 100000) {
-            return $this->numberToWords(floor($number / 1000)) . ' Thousand' . ($number % 1000 ? ' ' . $this->numberToWords($number % 1000) : '');
-        } elseif ($number < 10000000) {
-            return $this->numberToWords(floor($number / 100000)) . ' Lakh' . ($number % 100000 ? ' ' . $this->numberToWords($number % 100000) : '');
-        } else {
-            return $this->numberToWords(floor($number / 10000000)) . ' Crore' . ($number % 10000000 ? ' ' . $this->numberToWords($number % 10000000) : '');
         }
+
+        if ($number < 100) {
+            return $tens[10 * floor($number / 10)] . ($number % 10 ? ' ' . $ones[$number % 10] : '');
+        }
+
+        if ($number < 1000) {
+            return $ones[floor($number / 100)] . ' Hundred' . ($number % 100 ? ' ' . $this->convertNumberPortionToWords($number % 100) : '');
+        }
+
+        if ($number < 100000) {
+            return $this->convertNumberPortionToWords(floor($number / 1000)) . ' Thousand' . ($number % 1000 ? ' ' . $this->convertNumberPortionToWords($number % 1000) : '');
+        }
+
+        if ($number < 10000000) {
+            return $this->convertNumberPortionToWords(floor($number / 100000)) . ' Lakh' . ($number % 100000 ? ' ' . $this->convertNumberPortionToWords($number % 100000) : '');
+        }
+
+        return $this->convertNumberPortionToWords(floor($number / 10000000)) . ' Crore' . ($number % 10000000 ? ' ' . $this->convertNumberPortionToWords($number % 10000000) : '');
     }
 
     /**
