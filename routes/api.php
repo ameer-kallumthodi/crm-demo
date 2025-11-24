8faab477f2018b44fd4479075efcb22c39f2069f
Ameer Suhail <ameersuhail2001@gmail.com>
2025-11-24 17:50:46 +0530

    api route

---
+++ routes/api.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\HomeController;
+use App\Http\Controllers\API\LeadsController;
+use App\Http\Controllers\API\NotificationController;
+use App\Http\Controllers\API\MarketingLeadsController;
+use App\Http\Controllers\API\FollowupLeadsController;
+use App\Http\Controllers\API\RegistrationLeadsController;
+
+Route::prefix('v1')->group(function() {
+    Route::post('auth/login', [AuthController::class, 'login']);
+    
+    // Protected routes
+    Route::middleware('auth:sanctum')->group(function() {
+        Route::post('auth/logout', [AuthController::class, 'logout']);
+        Route::get('home', [HomeController::class, 'index']);
+        Route::get('marketing-home', [HomeController::class, 'marketingHome']);
+        Route::get('leads', [LeadsController::class, 'index']);
+        Route::get('leads/filters', [LeadsController::class, 'filters']);
+        Route::get('leads/call', [LeadsController::class, 'callLead']);
+        Route::get('notifications', [NotificationController::class, 'index']);
+        
+        // Marketing Leads APIs
+        Route::get('marketing-leads', [MarketingLeadsController::class, 'index']);
+        Route::post('marketing-leads', [MarketingLeadsController::class, 'store']);
+        Route::get('marketing-leads/d2d-form-messages', [MarketingLeadsController::class, 'd2dFormMessages']);
+        Route::get('marketing-leads/form-data', [MarketingLeadsController::class, 'formData']);
+        Route::get('marketing-leads/filters', [MarketingLeadsController::class, 'filters']);
+
+        // Follow-up Leads APIs
+        Route::get('followup-leads', [FollowupLeadsController::class, 'index']);
+        Route::get('followup-leads/filters', [FollowupLeadsController::class, 'filters']);
+
+        // Registration Form Submitted Leads APIs
+        Route::get('registration-leads', [RegistrationLeadsController::class, 'index']);
+        Route::get('registration-leads/filters', [RegistrationLeadsController::class, 'filters']);
+        Route::get('registration-leads/{lead}', [RegistrationLeadsController::class, 'show'])->whereNumber('lead');
+    });
+});
+
