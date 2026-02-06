79014b86388c8ff4167ee8a03118393ad802ed15
Ameer Suhail <ameersuhail2001@gmail.com>
2026-02-06 11:33:22 +0530

    b2b

--- resources---/views/admin/online-teaching-faculties/partials/actions.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="btn-group" role="group">
-    <a href="{{ route('admin.online-teaching-faculties.show', $faculty->id) }}" class="btn btn-outline-primary btn-sm" title="View">
-        <i class="ti ti-eye"></i>
-    </a>
-</div>
-
